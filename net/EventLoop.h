4aeed100c4d2d6a6056191923212c918cbafe28a
winston <670293316@qq.com>
2017-12-15 18:29:35 +0800

    eventloop

---
+++ net/EventLoop.h
@@ -0,0 +1,27 @@
+#ifndef _MUDUO_EVENTLOOP_H_
+#define _MUDUO_EVENTLOOP_H_
+#include "base/NonCopyable.h"
+#include "thread"
+
+namespace muduo{
+class EventLoop : noncopyable
+{
+public:
+    EventLoop();
+    ~EventLoop();
+    void loop();
+    void assertInLoopThread()
+    {
+        if( !isInLoopThread())
+        {
+            abortNotInLoopThread();
+        }
+    }
+    bool isInLoopThread() const { return threadId_ == std::this_thread::get_id();}
+private:
+    void abortNotInLoopThread();
+    bool looping_;
+    const std::thread::id threadId_;
+};
+};
+#endif//_MUDUO_EVENTLOOP_H_
